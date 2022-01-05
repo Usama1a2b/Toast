5285bd0c318d5e2c97edbd1e9db8630f98fa4661
usama <bitf18a534@pucit.edu.pk>
2022-01-05 16:33:29 +0500

    Toast

---
+++ app/src/main/java/com/example/customizedtoast/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.customizedtoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+     Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1=findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"first toast",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
