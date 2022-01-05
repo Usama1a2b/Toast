a73492f3c4b2e94dc46cdfb67a360620d8f0bc70
usama <bitf18a534@pucit.edu.pk>
2022-01-05 17:06:27 +0500

    custom toast done

--- app/src/main/java/com/example/customizedtoast/MainActivity.java
+++ app/src/main/java/com/example/customizedtoast/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.customizedtoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -27,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void showToast()
     {
+        LayoutInflater inflater=getLayoutInflater();
+        View layout=inflater.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.lay));
+
+        Toast toast =new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+
+        toast.show();
 
     }
 }
